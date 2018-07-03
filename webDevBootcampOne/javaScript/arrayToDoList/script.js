72e54f30682427286d7b4d9430991222d2d9563e
8bit <lizardscruff@users.noreply.github.com>
2018-07-03 15:33:04 +0900

    Add files via upload

--- webDevBootcampOne/javaScript/arrayToDoList/script.js
+++ webDevBootcampOne/javaScript/arrayToDoList/script.js
@@ -9,17 +9,42 @@ window.setTimeout(function() {
 while(choice !== "quit") {
 	//handle input
 	if (choice === "list") {
-		console.log(todos);
+		listTodos();
 	} else if (choice === "new") {
-		//ask for new todo
-		var newToDo = prompt("What todo would you like to add?");
-		//add new todo to array
-		todos.push(newToDo);
+		addTodo();
+	} else if (choice === "delete" ) {
+		deleteTodo();
 	}
-
 	//ask again for new input
 	var choice = prompt("What would you like to do?");
 }
 
 console.log("Ok, you quit the app! See ya!");
 }, 500);
+
+function listTodos() {
+	console.log("**********");
+	//within the function the todo is the item in the array
+	//index keeps the index number in it
+	todos.forEach(function(todo, index) {
+		console.log(index + ": " + todo);
+	});
+	console.log("**********");
+}
+
+function addTodo() {
+	//ask for new todo
+	var newToDo = prompt("What todo would you like to add?");
+	//add new todo to array
+	todos.push(newToDo);
+	console.log("Added Todo");
+}
+
+function deleteTodo() {
+	//ask for index of todo to be deleted
+	var index = prompt("Enter the index of the todo that you would like to delete.")
+	//delete that todo
+	//splice()
+	todos.splice(index, 1);
+	console.log("Deleted Todo");
+}
