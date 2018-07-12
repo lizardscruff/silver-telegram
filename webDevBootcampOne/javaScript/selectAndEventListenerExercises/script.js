286f5aa75835b298cbb703ff1400bf547baa4d74
Moogle <lizardscruff@users.noreply.github.com>
2018-07-13 07:21:16 +0900

    Add files via upload

--- webDevBootcampOne/javaScript/selectAndEventListenerExercises/script.js
+++ webDevBootcampOne/javaScript/selectAndEventListenerExercises/script.js
@@ -1,17 +1,38 @@
-
+// Zack's Solution
 var button = document.querySelector('button');
-var body = document.querySelector('body');
-var isOrange = false;
+// var body = document.querySelector('body');
+// var isOrange = false;
 
-button.addEventListener("click", itsAlive);
+// button.addEventListener("click", itsAlive);
 
-function itsAlive () {
+// function itsAlive () {
 	
-	if (isOrange === false) {
-		body.style.background = 'orange';
-		isOrange = true;
-	} else {
-		body.style.background = 'white';
-	}
+// 	if (isOrange === false) {
+// 		body.style.background = 'orange';
+// 		isOrange = true;
+// 	} else {
+// 		body.style.background = 'white';
+// 		isOrange = false;
+// 	}
+// }
 
-}
+
+
+//Colt's Solution repurposed
+// var isPurple = false;
+
+// button.addEventListener('click', function() {
+// 	if (isPurple) {
+// 		document.body.style.background = 'white';
+// 	} else {
+// 		document.body.style.background = 'purple';
+// 	}
+// 	isPurple = !isPurple;
+// });
+
+
+
+// look at the html
+button.addEventListener('click', function() {
+	document.body.classList.toggle("purple");
+});
