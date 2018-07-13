c9ac6c25e2870fbb35e8f34cb0f51baa34b80b42
Moogle <lizardscruff@users.noreply.github.com>
2018-07-13 15:47:14 +0900

    Add files via upload

--- webDevBootcampOne/javaScript/ScoreKeeper/script.js
+++ webDevBootcampOne/javaScript/ScoreKeeper/script.js
@@ -2,15 +2,21 @@ var p1Button = document.querySelector('#p1');
 var p2Button = document.getElementById('p2');
 var p1Display = document.querySelector('#p1Display');
 var p2Display = document.querySelector('#p2Display');
+var resetButton = document.getElementById('reset');
+var numInput = document.querySelector('input');
+var winningScoreDisplay = document.querySelector('p span');
 var p1Score = 0;
 var p2Score = 0;
 var gameOver = false;
 var winningScore = 5;
 
+//these first two event listeners can be refactored later and made DRY
+//we just are focusing on practicing event listeners right now
 p1Button.addEventListener('click', function() {
 	if (!gameOver) {
 		p1Score++;
 		if (p1Score === winningScore) {
+			p1Display.classList.add('winner');
 			gameOver = true;
 		}
 		p1Display.textContent = p1Score;
@@ -18,14 +24,33 @@ p1Button.addEventListener('click', function() {
 
 });
 
-
 p2Button.addEventListener('click', function() {
 	if (!gameOver) {
 		p2Score++;
 		if (p2Score === winningScore) {
+			p2Display.classList.add('winner');
 			gameOver = true;
 		}
 	}
 	p2Display.textContent = p2Score;
 });
 
+resetButton.addEventListener('click', function(){
+	reset();
+});
+
+function reset(){
+	p1Score = 0;
+	p2Score = 0;
+	p1Display.textContent = 0;
+	p2Display.textContent = 0;
+	p1Display.classList.remove('winner');
+	p2Display.classList.remove('winner');
+	gameOver = false;	
+}
+
+//inside the function you can change numInput to the 'this' keyword
+numInput.addEventListener('change', function(){
+	winningScoreDisplay.textContent = numInput.value;
+	winningScore = Number(numInput.value);
+});
