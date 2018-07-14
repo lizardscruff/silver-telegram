01d94163b7219d2a1dcc3ce487d2b6eca1feacbb
Moogle <lizardscruff@users.noreply.github.com>
2018-07-14 10:04:38 +0900

    Add files via upload

--- webDevBootcampOne/javaScript/otherEventsTodoList/script.js
+++ webDevBootcampOne/javaScript/otherEventsTodoList/script.js
@@ -1,5 +1,13 @@
-var firstLI = document.querySelector('li');
+var lis = document.querySelectorAll('li');
 
-firstLI.addEventListener('mouseover', function() {
-	console.log('mousedover!');
-});
+for (var i = 0; i < lis.length; i++) {
+	lis[i].addEventListener('mouseover', function() {
+		this.classList.add('selected');
+	});
+	lis[i].addEventListener('mouseout', function() {
+		this.classList.remove('selected');
+	});
+	lis[i].addEventListener('click', function() {
+		this.classList.toggle('done');
+	});
+}
