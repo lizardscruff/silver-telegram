6f45955ee267ba84c2f9586277c5bf6efbf8f600
Moogle <lizardscruff@users.noreply.github.com>
2018-07-16 06:29:06 +0900

    Add files via upload

--- webDevBootcampOne/javaScript/otherEventsTodoList/script.js
+++ webDevBootcampOne/javaScript/otherEventsTodoList/script.js
@@ -1,5 +1,19 @@
 var lis = document.querySelectorAll('li');
 
+// for (var i = 0; i < lis.length; i++) {
+// 	lis[i].addEventListener('mouseover', function() {
+// 		this.style.color = 'red';
+// 	});
+// 	lis[i].addEventListener('mouseout', function() {
+// 		this.style.color = 'black';
+// 	});
+// 	lis[i].addEventListener('click', function() {
+// 		this.classList.toggle('done');
+// 	});
+
+// }
+
+//this is better practice because it keeps the styling in the css file
 for (var i = 0; i < lis.length; i++) {
 	lis[i].addEventListener('mouseover', function() {
 		this.classList.add('selected');
