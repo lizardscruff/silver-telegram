1afae30e0ded28f67ece619976102a1ebc58ced9
Moogle <lizardscruff@users.noreply.github.com>
2018-07-31 21:21:58 +0900

    Add files via upload

---
+++ webDevBootcampOne/jQuery/Effects/effects.js
@@ -0,0 +1,29 @@
+$('button').on('click', function(){
+	$('div').slideToggle(1000, function(){
+		console.log('slide is done!');
+	});
+});
+
+// $('button').on('click', function(){
+// 	$('div').slideUp();
+// });
+
+// $('button').on('click', function(){
+// 	$('div').slideDown();
+// });
+
+// $('button').on('click', function(){
+// 	$('div').fadeToggle(500);
+// });
+
+// $('button').on('click', function(){
+// 	$('div').fadeOut(1000, function(){
+// 		$(this).remove();
+// 	});
+// });
+
+// $('button').on('click', function(){
+// 	$('div').fadeIn(1000, function(){
+// 		$(this).remove();
+// 	});
+// });
